c2ca48eb7a6159d692bdb4f421cb5568219724d9
Даниил <puiuld47@gmail.com>
2025-02-02 20:06:50 +0200

    Task2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19._2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Вывести первые пять членов арифметической прогрессии (с использованием цикла) 
+        /// и найти их общую сумму, если a1=1, d=4 (например, 1  5  9  13  17, sum=45).
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int a1 = 1;
+            int d = 4;
+            int n = 5;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = a1 + i * d;
+                Console.Write(num + " ");
+                sum += num;
+            }
+
+            Console.WriteLine($"Сумма:{sum}");
+            Console.ReadLine();
+        }
+    }
+}
